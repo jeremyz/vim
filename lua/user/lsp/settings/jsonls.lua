2b26c97730c28541e994bff24a2640238f98e2c9
Jérémy Zurcher <jeremy@asynk.ch>
2022-03-15 16:20:18 +0100

    lsp jsonls : return on error

--- lua/user/lsp/settings/jsonls.lua
+++ lua/user/lsp/settings/jsonls.lua
@@ -1,6 +1,8 @@
 local default_schemas = nil
 local status_ok, jsonls_settings = pcall(require, "nlspsettings.jsonls")
-if status_ok then
+if not status_ok then
+  return
+else
   default_schemas = jsonls_settings.get_default_schemas()
 end
 
