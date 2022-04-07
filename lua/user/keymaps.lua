a2d47a73922822386d9f8b0d61903789166ff14f
Jérémy Zurcher <jeremy@asynk.ch>
2022-04-07 15:33:37 +0200

    keymaps : fix buffer mappings

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -42,8 +42,8 @@ keymap("n", "<C-Left>", ":vertical resize +2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize -2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-Q>", ":bnext<CR>", opts)
-keymap("n", "<S-H>", ":bprevious<CR>", opts)
+keymap("n", "<S-H>", ":bnext<CR>", opts)
+keymap("n", "<S-Q>", ":bprevious<CR>", opts)
 
 -- Move text up and down
 -- keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
