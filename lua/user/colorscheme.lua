f2c6e2a799a383658daaf14f7f1468fc64963ddc
Jérémy Zurcher <jeremy@asynk.ch>
2022-10-14 11:29:31 +0200

    add IncSearch in colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -6,6 +6,7 @@ colorscheme darkplus
 hi CursorLineNr guibg=#8a4a59
 hi CursorLine guibg=#3a3a3a
 hi ColorColumn guibg=#3a3a3a
+hi IncSearch guibg=peru guifg=wheat
 hi Search guibg=peru guifg=wheat
 hi NonText ctermfg=16 guifg=#4a4a59
 hi ExtraWhitespace ctermbg=red guibg=red ctermfg=black guifg=black
